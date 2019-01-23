3d284d7014ded990d4b44f9a04fa310a34e58a85
Pavel Bazhan <spartaktxd@gmail.com>
2019-01-23 22:37:47 +0300

    code-001

---
+++ code-001/README.md
@@ -0,0 +1,3 @@
+1. Произвести дешифровку: `yarn decrypt`
+2. Произвести шифрование: `yarn encrypt`
+3. ОТВЕТ: [result](https://github.com/PavelBazhan/ifmo-results-3/tree/master/code-001/result)
---
+++ code-001/index.js
@@ -0,0 +1,7 @@
+/*Pavel Bazhan*/
+const crypto = require('crypto');
+const fs = require('fs');
+var key = fs.readFileSync('./key');
+var buffer = fs.readFileSync('./secret');
+var answer = crypto.publicDecrypt(key, buffer);
+console.log(answer.toString());
