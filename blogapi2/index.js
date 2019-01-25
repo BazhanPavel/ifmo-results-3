1db1f9af2871ebbe8d7f0d14e255b8ae0eb0ec2f
Pavel Bazhan <spartaktxd@gmail.com>
2019-01-25 16:10:48 +0300

    pug template

--- blogapi2/index.js
+++ blogapi2/index.js
@@ -5,13 +5,13 @@ let posts = [
   { id: 1,
   title: 'Hi!',
   categories: 'Computer, Friends',
-  content: 'Post about Friends'
+  content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non sodales ligula, sed tristique metus. Duis ultrices velit nisl, congue pretium quam tempus in. Vestibulum finibus sed augue in ultrices. Duis pulvinar faucibus tortor vitae pharetra. Curabitur tincidunt, nisi ut cursus rhoncus, odio sapien gravida dolor, a pulvinar eros dolor ac mi.'
   },
   {
   id: 2,
   title: 'New Post',
   categories: 'Candy',
-  content: 'Post about Candy'
+  content: 'Vivamus tincidunt fermentum justo, et malesuada urna pellentesque vel. Aenean elementum cursus feugiat. Donec volutpat felis orci, a egestas mauris mollis eget. Praesent feugiat metus ac purus ultrices, a ultricies nisi cursus. Duis lacinia mauris magna, vel accumsan nisl euismod eget. Nullam at enim interdum, cursus urna rhoncus, eleifend ligula.'
   }
 ]
 
@@ -19,6 +19,9 @@ app.listen(1234);
 app
     .set('view engine', 'pug')
     .set('views', __dirname + '/views')
+    .get('/public/:file', (req, res) => {
+      res.sendFile(`${__dirname}/public/${req.params.file}`);
+    })
     .get('/', (req, res) => {
       res.render('index', {posts: posts});
     })
