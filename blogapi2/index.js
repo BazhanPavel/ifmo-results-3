bb15e4f74f31fbfc1f4815ef8fdce34d34ce249d
Pavel Bazhan <spartaktxd@gmail.com>
2019-01-25 20:52:34 +0300

    mongose basic

--- blogapi2/index.js
+++ blogapi2/index.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const {
+  performance
+} = require('perf_hooks');
+mongoose.connect("mongodb://localhost:27017/postsdb", { useNewUrlParser: true });
 
 const postScheme = new mongoose.Schema({
   id: Number,
   title: String,
   categories: String,
   content: String
-});
+}, { collection: 'posts'});
 const Post = mongoose.model("Post", postScheme);
 
-let posts = [
+let posts;
+
+posts = [
   { id: 1,
   title: 'Hi!',
   categories: 'Computer, Friends',
@@ -25,36 +32,54 @@ let posts = [
   }
 ]
 
+
+
 app.listen(1234);
 app
     .set('view engine', 'pug')
     .set('views', __dirname + '/views')
+    .use(bodyParser.json())
+    .use(bodyParser.urlencoded({ extended: true }))
     .get('/public/:file', (req, res) => {
       res.sendFile(`${__dirname}/public/${req.params.file}`);
     })
     .get('/', (req, res) => {
-      res.render('index', {posts: posts});
+
+      time = performance.now();
+      Post.find({}, (err, docs) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        time = performance.now() - time;
+        console.log(time);
+        time = performance.now();
+        res.render('index', {posts: docs});
+        time = performance.now() - time;
+        console.log(time + '\n');
+      });
+
     })
     .get('/new_post', (req, res) => {
       res.render('new_post');
     })
     .get('/api/posts', (req, res) => {
-      mongoose.connect("mongodb://localhost:27017/postsdb", { useNewUrlParser: true });
-      let post = new Post({
-        id: 1,
-        title: 'String',
-        categories: 'String',
-        content: 'String'
-      });
-      post.save(err => {
-        mongoose.disconnect();  // отключение от базы данных
-        if(err) return console.log(err);
-        console.log("Сохранен объект", user);
-      });
       res.send(posts);
     })
     .post('/api/posts', (req, res) => {
-      res.send('post is created');
+      mongoose.connect("mongodb://localhost:27017/postsdb", { useNewUrlParser: true });
+      console.log(req.body.content);
+      let post = new Post({
+        id: 1,
+        title: req.body.title,
+        categories: req.body.categories,
+        content: req.body.content
+      });
+      post.save(err => {
+        if(err) return console.log(err);
+        console.log("Сохранен объект\n", post);
+      });
+      res.redirect('/');
     })
     .get('/api/posts/:id', (req, res) => {
       res.send(`post #${req.params.id}`);
--- blogapi2/public/submit.js
+++ blogapi2/public/submit.js
@@ -1,8 +1,6 @@
 document.getElementById('textarea').addEventListener('keypress', e => {
   if(e.which == 13 && !e.shiftKey) {
-    //$(this).closest("form").submit();
     document.getElementById('mainform').submit();
-    //console.log(document.getElementById('mainform'));
     e.preventDefault();
     return false;
   }
